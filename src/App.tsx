df95512fd4eb5c8c55ca86c06a00ee94f3259673
ballalthrusts <aniruddhballaldeeksha@gmail.com>
2025-07-12 23:06:47 +0530

    Removing Layout.tsx-6

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,18 @@ import Publications from './pages/Publications';
 import Support from './pages/Support';
 import People from './pages/People';
 import Contact from './pages/Contact';
-import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/research" element={<Research />} />
-          <Route path="/publications" element={<Publications />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/people" element={<People />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="/publications" element={<Publications />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
